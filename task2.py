5f90f517d298d3fd4451c87531e530e589c9cffb
AndriiPikulia <145601915+AndriiPikulia@users.noreply.github.com>
2023-09-22 14:57:37 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,16 @@
+
+
+
+print("A + B / C ** D")
+print("Введіть А")
+a = int(input())
+print("Введіть B")
+b = int(input())
+print("Введіть C")
+
+c = int(input())
+print("Введіть D")
+d = int(input())
+
+print("result:")
+print(a + b /c ** d)
